f4fb1ab08f32b7734f0aa4eeaf0b47a707110488
DaniilT <tarasov-danil@mail.ru>
2026-04-18 12:25:05 +0500

    добавил избранное

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 function ProductCard(props) {
   const product = props.product;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function toggleFavorite() {
+    setIsFavorite(!isFavorite);
+  }
 
   return (
     <div>
-      <h2>{product.make}</h2>
-      <p>{product.model}</p>
+      <img src={product.images[0]} alt={product.model} width="200" />
+
+      {product.isSpecialOffer === true && <p>Special Offer</p>}
+
+      <p>{product.make}</p>
+      <h2>{product.model}</h2>
       <p>${product.price}</p>
+
+      <button onClick={toggleFavorite}>
+        {isFavorite ? "♥ In Favorites" : "♡ Add to Favorites"}
+      </button>
     </div>
   );
 }
